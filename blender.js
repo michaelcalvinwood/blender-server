1dbcbfca2640fad24b52329f954db489abfcf09a
Michael Wood <michaelwood33311@icloud.com>
2023-06-07 07:52:52 -0400

    added receiving mix from client

--- blender.js
+++ blender.js
@@ -18,6 +18,15 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+
+const processMix = async (mix, socket) => {
+  console.log('mix', mix);
+}
+
+const handleSocketEvents = async socket => {
+  socket.on('mix', (mix) => processMix(mix, socket))
+}
+
 const httpsServer = https.createServer({
     key: fs.readFileSync(privateKeyPath),
     cert: fs.readFileSync(fullchainPath),
@@ -28,22 +37,30 @@ const httpsServer = https.createServer({
     console.log(`HTTPS Server running on port ${listenPort}`);
 });
 
-var io = socketio(httpsServer);
+const io = require('socket.io')(httpsServer, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      'https://blender.pymnts.com'
+    ],
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on('connection', (socket) => {
-  console.log(socket.request.session);
+  
   // if(socket.request.session.name !== undefined){
   //   socket.emit('name', socket.request.session.name); // notice socket.io has access to session.name
   //   io.emit('event', socket.request.session.name + ' has joined!');
   // }
 
   console.log('connected', socket.id);
-  
-  socket.on('name', (name) => {
-    socket.request.session.name = name; // add name to the session object
-    socket.request.session.save(); // save the session object to persist through other requests
-    socket.broadcast.emit('event', name + ' says hello!');
-  });
+  handleSocketEvents(socket);
+  // socket.on('name', (name) => {
+  //   socket.request.session.name = name; // add name to the session object
+  //   socket.request.session.save(); // save the session object to persist through other requests
+  //   socket.broadcast.emit('event', name + ' says hello!');
+  // });
 });
 
 
