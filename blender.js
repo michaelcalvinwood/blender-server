bdcedb5c5cc92fc5e5d7e8c6f4f9b94cfc652c72
Michael Wood <michaelwood33311@icloud.com>
2023-06-07 07:30:55 -0400

    added socket.io

--- blender.js
+++ blender.js
@@ -7,6 +7,7 @@ const express = require('express');
 const https = require('https');
 const cors = require('cors');
 const fs = require('fs');
+const socketio = require('socket.io');
 
 const app = express();
 app.use(express.static('public'));
@@ -25,6 +26,24 @@ const httpsServer = https.createServer({
 
   httpsServer.listen(listenPort, '0.0.0.0', () => {
     console.log(`HTTPS Server running on port ${listenPort}`);
+});
+
+var io = socketio(httpsServer);
+
+io.on('connection', (socket) => {
+  console.log(socket.request.session);
+  // if(socket.request.session.name !== undefined){
+  //   socket.emit('name', socket.request.session.name); // notice socket.io has access to session.name
+  //   io.emit('event', socket.request.session.name + ' has joined!');
+  // }
+
+  console.log('connected', socket.id);
+  
+  socket.on('name', (name) => {
+    socket.request.session.name = name; // add name to the session object
+    socket.request.session.save(); // save the session object to persist through other requests
+    socket.broadcast.emit('event', name + ' says hello!');
+  });
 });
 
 
